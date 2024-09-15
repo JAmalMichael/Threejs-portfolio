f2cce1aad0adb750f8d62c4b599e39804a49c7f7
JAmalMichael <eshimi090@gmail.com>
2024-09-15 05:36:55 -1200

    Contact component and emailjs setup

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,129 @@
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const formRef = useRef();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target; // Accessing the target of the event (input field)
+    setForm({ ...form, [name]: value }); // Setting the form state
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    try {
+      await emailjs.send(
+        "service_74qheai",
+        "template_4mibdii",
+        {
+          from_name: form.name,
+          to_name: "JAmalMichael",
+          from_email: form.email,
+          to_email: "eshimi090@gmail.com",
+          message: form.message,
+        },
+        "7EQKqV0dBTP9wfJaN"
+      );
+      setLoading(false);
+
+      alert("Your message has been sent");
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      setLoading(false);
+
+      console.log(error);
+
+      alert("Something went wrong");
+    }
+  };
+
+  //   service_74qheai, template_pjofozp
+
+  return (
+    <section className="c-space my-20" id="contact">
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <img
+          src="/assets/terminal.png"
+          alt="terminal background"
+          className="absolute inset-0 min-h-screen"
+        />
+        <div className="contact-container">
+          <h3 className="head-text">Contact Me</h3>
+          <p className="text-lg text-white-600 mt-3">
+            Whether you're launching a new website, enhancing your current
+            platform, or bringing a unique project to life, I'm ready to help
+            you create something extraordinary.
+          </p>
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-12 flex flex-col space-y-7"
+          >
+            <label className="space-y-3">
+              <span className="field-label">Full Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., John Doe"
+              />
+            </label>
+
+            <label className="space-y-3">
+              <span className="field-label">Email address</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., johndoe@gmail.com"
+              />
+            </label>
+
+            <label className="space-y-3">
+              <span className="field-label">Your message</span>
+              <textarea
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                required
+                rows={5}
+                className="field-input"
+                placeholder="Share your thoughts or inquiries..."
+              />
+            </label>
+            <button className="field-btn" type="submit" disabled={loading}>
+              {loading ? "Sending..." : "Send Message"}
+              <img
+                src="/assets/arrow-up.png"
+                alt="arrow-up"
+                className="field-btn_arrow"
+              />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
